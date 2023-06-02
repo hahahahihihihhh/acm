7505cb40f381710f95735d3edac8ef1eec785f41
hahahahihihihhh <1249695730@qq.com>
2023-06-02 22:15:32 +0800

    Changes

---
+++ acm_problem/atcoder/abc303_A.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 86159 on 2023/6/2.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline void solve()
+{
+    int n;  cin >> n;
+    string s, t;    cin >> s >> t;
+    for (int i = 0; i < n; i ++){
+//        char a = s[i], b = t[i];
+        if (s[i] == '1' || s[i] == 'l')    s[i] = '1';
+        if (t[i] == '1' || t[i] == 'l')    t[i] = '1';
+        if (s[i] == '0' || s[i] == 'o')    s[i] = '0';
+        if (t[i] == '0' || t[i] == 'o')    t[i] = '0';
+    }
+    cout << (s == t ? "Yes" : "No") << "\n";
+}
+
+int main(){
+//    int T;  cin >> T;
+//    while (T--)
+        solve();
+    return 0;
+}
--- acm_problem/atcoder/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 86159 on 2023/6/2.
-//
-
-#include <iostream>
-
-int main() {
-    std::cout << "hello world" << "\n";
-}
